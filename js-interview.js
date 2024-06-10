a306360f507f97297f404804f16d8f07fe88b72b
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-10 15:24:51 +0530

    remove duplicates from array

--- js-interview.js
+++ js-interview.js
@@ -3,7 +3,6 @@
 function abc() {
   console.log(this);
 }
-
 abc();
 
 //flatten an array using for of
@@ -26,3 +25,23 @@ const toFlatten = (params) => {
 };
 
 console.log(toFlatten(arr));
+
+
+//write a function in javascript which remove duplicates from array
+//Method 1
+
+let arr1 = [1,2,3,4,5,5,6,2,7,3];
+
+let arr1Result = () => {
+  return [...new Set(arr1)] 
+}
+console.log(arr1Result());
+
+//Method 2
+
+let arr2Result = () => {
+  return arr1.filter(function(item, pos){
+    return arr1.indexOf(item) == pos;
+  })
+}
+console.log(arr2Result());
