0a819e247a26f44800039277817b9ccd5d2e9161
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-10 15:41:42 +0530

    Fibonacci series

--- js-interview.js
+++ js-interview.js
@@ -45,3 +45,22 @@ let arr2Result = () => {
   })
 }
 console.log(arr2Result());
+
+// Fibonacci Series
+
+let number = parseInt(prompt("enter number"));
+let n1=0, n2=1, next;
+
+let printFb = () => {
+  for (let i=0; i< number; i++){
+    console.log(n1);
+
+    next = n1 + n2;
+    n1 = n2;
+    n2 = next;
+  }
+}
+
+printFb();
+
+
