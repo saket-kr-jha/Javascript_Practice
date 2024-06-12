415f463af039148e624448862c06bd3a56c7c71c
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-12 22:36:13 +0530

    added check palindrome function

--- practiceQuestions/js-interview.js
+++ practiceQuestions/js-interview.js
@@ -119,5 +119,6 @@ createChunk([1,2,3,4,5,6,7,8,9,10,11,12,13,14], 3);
 
 // check if palindrome
 (function palindrome(){
-  
+  let number = 1223221;
+  const result = checkPalindrome(number);
 })()
