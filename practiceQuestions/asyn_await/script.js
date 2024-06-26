0461d98fe4b7df7130973df5afb3068cb824e284
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-26 21:50:15 +0530

    async

--- practiceQuestions/asyn_await/script.js
+++ practiceQuestions/asyn_await/script.js
@@ -1,1 +1,12 @@
+//async code
 console.log("hello");
+function importantAction(username, cb){
+    setTimeout(()=>{
+        cb(`Subscribe to ${username}`);
+    },0)
+}
+
+const message = importantAction("Saket", (message)=>{console.log(message)})
+
+
+console.log("Last console")
