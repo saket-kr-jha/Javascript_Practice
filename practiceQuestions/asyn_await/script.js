ddfebedfc74e97e22528507e62202ca0d501ce25
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-27 11:42:23 +0530

    promise chaining

--- practiceQuestions/asyn_await/script.js
+++ practiceQuestions/asyn_await/script.js
@@ -11,21 +11,67 @@
 
 // promises
 
+// console.log("hello");
+// const sub = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const result = false;
+//     if (result) resolve("Hello from promise");
+//     else reject(new Error("Error from set timeout"));
+//   }, 2000);
+// });
+
+// console.log(sub);
+
+// sub.then((res) => {
+//     console.log("Resolved");
+//   })
+//   .catch((err) => {
+//     console.log("Error");
+//   });
+// console.log("Last console");
+
+// promise chaining
+
 console.log("hello");
-const sub = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    const result = false;
-    if (result) resolve("Hello from promise");
-    else reject(new Error("Error from set timeout"));
-  }, 2000);
-});
 
-console.log(sub);
+function importantAction(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Hi my name is ${username}`);
+    }, 1000);
+  });
+}
 
-sub.then((res) => {
-    console.log("Resolved");
+function likeIt(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`I Like ${name}`);
+    }, 1000);
+  });
+}
+
+function desc(desg) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`My desc is ${desg}`);
+    }, 1000);
+  });
+}
+
+importantAction("Saket")
+  .then((res) => {
+    console.log(res);
+    return likeIt("Mango");
+  })
+  .then((res) => {
+    console.log(res);
+    return desc("student");
+  })
+  .then((res) => {
+    console.log(res);
   })
   .catch((err) => {
     console.log("Error");
   });
-console.log("Last console");
+
+  console.log("LAst")
