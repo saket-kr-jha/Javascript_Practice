0893710ea3e9b321f63017e4b398bf7da74402c0
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-27 11:08:03 +0530

    promises

--- practiceQuestions/asyn_await/script.js
+++ practiceQuestions/asyn_await/script.js
@@ -1,12 +1,31 @@
 //async code
+// console.log("hello");
+// function importantAction(username, cb){
+//     setTimeout(()=>{
+//         cb(`Subscribe to ${username}`);
+//     },0)
+// }
+
+// const message = importantAction("Saket", (message)=>{console.log(message)})
+// console.log("Last console")
+
+// promises
+
 console.log("hello");
-function importantAction(username, cb){
-    setTimeout(()=>{
-        cb(`Subscribe to ${username}`);
-    },0)
-}
+const sub = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const result = false;
+    if (result) resolve("Hello from promise");
+    else reject(new Error("Error from set timeout"));
+  }, 2000);
+});
 
-const message = importantAction("Saket", (message)=>{console.log(message)})
+console.log(sub);
 
-
-console.log("Last console")
+sub.then((res) => {
+    console.log("Resolved");
+  })
+  .catch((err) => {
+    console.log("Error");
+  });
+console.log("Last console");
